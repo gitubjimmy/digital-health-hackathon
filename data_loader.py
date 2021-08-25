d13c5e8d0b9aa11d62196b6337215fa2fad28546
gitubjimmy <jimmynew1130@gmail.com>
2021-08-26 02:21:37 +0900

    minor fixes

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -5,6 +8,9 @@ from torchvision.datasets import CIFAR10
 
 
 def get_data_loaders():
+    # disable stdout
+    sys.stdout = open(os.devnull, 'w')
+
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -17,4 +23,6 @@ def get_data_loaders():
     test_set = CIFAR10(root='./data', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
 
+    # enable stdout
+    sys.stdout = sys.__stdout__
     return train_loader, test_loader
