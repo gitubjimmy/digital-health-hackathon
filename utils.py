54f27df9d8c0b0c280385938b0188a12c8ab086f
kdha0727 <kdha0727@naver.com>
2021-09-05 09:52:10 +0900

    fix trivial 3: more accurate line count

--- utils.py
+++ utils.py
@@ -10,18 +10,20 @@ import numpy as np
 from config import OUTPUT_FILE_PATH
 
 
-def img_show(img, filename):
+def img_show(img, filename: str):
     img = img / 2 + 0.5
     np_img = img.numpy()
     plt.imshow(np.transpose(np_img, (1, 2, 0)))
     plt.savefig(filename)
 
 
-def file_output(line):
+def file_output(data: str):
     open_method = 'a' if file_output.count else 'w'
-    file_output.count += 1
+    lines = data.splitlines()
     with open(OUTPUT_FILE_PATH, open_method) as output_file:
-        output_file.write(f'{line}\n')
+        for line in lines:
+            output_file.write(f'{line}\n')
+    file_output.count += len(lines)
 
 
 @contextlib.contextmanager
