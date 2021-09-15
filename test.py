27db5d9a6d0177f07e7b2f7103cb690ff6409190
kdha0727 <kdha0727@naver.com>
2021-09-16 02:57:25 +0900

    add tau score

---
+++ test.py
@@ -0,0 +1,83 @@
+import torch
+
+
+def get_tau_input(num_gene, has_gene, treatment):
+
+    assert isinstance(num_gene, int) and 1 <= num_gene <= 300
+    assert has_gene in (0, 1)  # allowed bool (True, False): bool is subclass of integer
+    assert treatment in (0, 1)  # allowed bool (True, False): bool is subclass of integer
+
+    import torch
+
+    try:
+        get_tau_input.cache
+    except AttributeError:
+        from data_prep_utils import get_processed_data
+        get_tau_input.cache = get_processed_data().samples.mean(axis=0).drop('time')
+
+    df = get_tau_input.cache.copy()
+    df['Treatment'] = int(treatment)
+    df['G{}'.format(num_gene)] = int(has_gene)
+    return torch.from_numpy(df.to_numpy())
+
+
+@torch.no_grad()
+def test():
+
+    import os
+    import pandas as pd
+    import torch
+
+    from models import get_model
+
+    net = get_model()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    net.load_state_dict(torch.load(os.path.join('checkpoint', "best_model_weight.pt")))
+    net.eval().to(device)
+
+    # t1_input: treatment == 1
+    # t2_input: treatment == 0
+
+    t1_pos_inputs = []
+    t1_neg_inputs = []
+    t2_pos_inputs = []
+    t2_neg_inputs = []
+
+    for g in range(1, 300 + 1):
+        t1_pos_inputs.append(get_tau_input(g, has_gene=1, treatment=1))
+        t1_neg_inputs.append(get_tau_input(g, has_gene=0, treatment=1))
+        t2_pos_inputs.append(get_tau_input(g, has_gene=1, treatment=0))
+        t2_neg_inputs.append(get_tau_input(g, has_gene=0, treatment=0))
+
+    t1_pos_tensor = torch.stack(t1_pos_inputs).to(torch.float32).to(device)
+    t1_neg_tensor = torch.stack(t1_neg_inputs).to(torch.float32).to(device)
+    t2_pos_tensor = torch.stack(t2_pos_inputs).to(torch.float32).to(device)
+    t2_neg_tensor = torch.stack(t2_neg_inputs).to(torch.float32).to(device)
+
+    tau1_score = torch.abs(net(t1_pos_tensor) - net(t1_neg_tensor)).squeeze()
+    tau2_score = torch.abs(net(t2_pos_tensor) - net(t2_neg_tensor)).squeeze()
+    tau_score = tau1_score - tau2_score
+
+    arr = torch.stack([tau1_score, tau2_score, tau_score]).transpose(0, 1).cpu().numpy()
+    df = pd.DataFrame(
+        arr,
+        index=tuple(map(lambda key: "G" + str(key), range(1, arr.shape[0] + 1))),
+        columns=("tau1", "tau2", "score(t1-t2)")
+    )
+
+    print("- - -")  # line
+    print()
+    print("**Tau Score**\n")
+    print("* Sort by tau1\n")
+    print(df.sort_values(by="tau1", ascending=False).to_markdown(tablefmt="grid"))
+    print()
+    print("* Sort by tau2\n")
+    print(df.sort_values(by="tau2", ascending=False).to_markdown(tablefmt="grid"))
+    print()
+
+    return df
+
+
+if __name__ == '__main__':
+    test()
--- train.py
+++ train.py
@@ -6,14 +6,15 @@ def train():
     import matplotlib.pyplot as plt
     from sklearn.metrics import r2_score
     from sklearn.model_selection import KFold
-    import torch.utils.data
     import torch
+    import torch.nn.functional as f
+    from torch.utils.data import SubsetRandomSampler
     import torchinfo
 
     import config
     from models.model import get_model
     from data_prep_utils import get_processed_data, get_loader
-    from train_utils import RegressionTrainer
+    from train_utils import RegressionTrainer, visualize_regression, visualize_learning
     from utils import file_output
 
     dataset = get_processed_data()
@@ -26,58 +27,49 @@ def train():
     optimizer_class = torch.optim.Adam
     optimizer_options = dict(lr=1e-3)
     scheduler_class = torch.optim.lr_scheduler.StepLR
-    scheduler_options = dict(step_size=10)
-    fold_count = config.NUM_K_FOLD
+    scheduler_options = dict(step_size=10, gamma=1e-1 ** 0.5)
+    num_folds = config.NUM_K_FOLD
     num_epochs = config.EPOCH_PER_K_FOLD
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     checkpoint_dir = 'checkpoint'
     os.makedirs(checkpoint_dir, exist_ok=True)
 
-    def initialize_trainer(fold, train_loader=None, val_loader=None):
+    def initialize_trainer(fold_count):
         n = get_model()
         o = optimizer_class(n.parameters(), **optimizer_options)
         s = scheduler_class(o, **scheduler_options)
         t = RegressionTrainer(
             n, criterion, o, s, epoch=num_epochs,
-            snapshot_dir=os.path.join(checkpoint_dir, f"fold_{fold}"),
-            train_iter=train_loader, val_iter=val_loader,
+            snapshot_dir=os.path.join(checkpoint_dir, f"fold_{fold_count}"),
+            # train_iter=train_loader, val_iter=val_loader,
             log_interval=1, progress=False
         )
-        # t.to(torch.float32)
         t.to(device)
         return t
 
-    kf = KFold(n_splits=fold_count, random_state=0, shuffle=True)
+    kf = KFold(n_splits=num_folds, random_state=0, shuffle=True)
     result = {}
 
     for fold, (train_idx, val_idx) in enumerate(kf.split(dataset)):
+
         print(f"\n<Fold {fold}>")
 
-        train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
-        val_sampler = torch.utils.data.SubsetRandomSampler(val_idx)
-        train_loader = get_loader(dataset, sampler=train_sampler)
-        val_loader = get_loader(dataset, sampler=val_sampler)
+        train_loader = get_loader(dataset, sampler=SubsetRandomSampler(train_idx))
+        val_loader = get_loader(dataset, sampler=SubsetRandomSampler(val_idx))
 
-        with initialize_trainer(fold, train_loader, val_loader) as trainer:
-            fold_result = trainer.run(train_loader, val_loader)
-            fold_result = np.array(fold_result)
-            train_result = list(fold_result[:, 0])
-            test_result = list(fold_result[:, 1])
-            best_loss = trainer.best_loss
-            result[fold] = dict(train_result=train_result, test_result=test_result, best_loss=best_loss)
-            # Plot Learning Curve to check overfitting
-            plt.figure(figsize=(12, 12))
-            plt.plot(range(1, num_epochs + 1), train_result, label='Training Loss')
-            plt.plot(range(1, num_epochs + 1), test_result, label='Test Loss')
-            min_val_loss = test_result.index(min(test_result)) + 1
-            plt.axvline(min_val_loss, linestyle='--', color='r', label='Early Stopping Checkpoint')
-            plt.title(f"Fold {fold} Learning Curve")
-            plt.xlabel("epochs")
-            plt.ylabel("loss")
-            plt.grid(True)
-            plt.legend()
-            plt.savefig(f"output_train_fold_{fold}.png")
+        with initialize_trainer(fold) as fitter:  # fixme: with 로 이제 안감싸도됨
+            train_result, test_result = fitter.fit(train_loader, val_loader, split_result=True)
+            result[fold] = dict(
+                train_result=train_result, test_result=test_result,
+                best_loss=fitter.best_loss, early_stopping=test_result.index(min(test_result)) + 1
+            )
+            # Plot Learning Curve to check over-fitting
+            visualize_learning(
+                train_result, test_result,
+                title=f"Fold {fold} Learning Curve", figsize=(12, 12),
+                filename=f"output_train_fold_{fold}.png", show=False
+            )
 
     best_fold = 0
     best_loss = float('inf')
@@ -87,60 +79,45 @@ def train():
             best_loss = fold_result['best_loss']
 
     checkpoint = glob.glob(os.path.join(checkpoint_dir, f'fold_{best_fold}', 'best_checkpoint_epoch_*.pt'))[-1]
+    model_state_dict = torch.load(checkpoint)['model']
+    torch.save(model_state_dict, os.path.join(checkpoint_dir, "best_model_weight.pt"))
 
     net = get_model()
-    net.load_state_dict(torch.load(checkpoint)['model'])
-    net.to(device)
+    net.load_state_dict(model_state_dict)
+    net.eval().to(device)
 
     with torch.no_grad():
         prediction = []
-        Y_train_r2 = []
-        running_loss = 0.0
+        label = []
+        mse = mae = 0.
 
-        loader = get_loader(dataset, train=False)
-        for inputs, labels in loader:
+        for inputs, labels in get_loader(dataset, train=False):
             inputs, labels = inputs.to(device), labels.to(device)
-            # inputs = inputs.to(torch.float32)
             outputs = net(inputs)
-            loss = criterion(outputs, labels)
+            mse += f.mse_loss(outputs, labels).item() * labels.shape[0]
+            mae += f.l1_loss(outputs, labels).item() * labels.shape[0]
             prediction.append(outputs)
-            Y_train_r2.append(labels)
-            running_loss += loss.item() * labels.shape[0]
+            label.append(labels)
 
-        prediction = torch.cat(prediction, dim=0).view(-1, 1).numpy()
-        Y_train_r2 = torch.cat(Y_train_r2, dim=0).view(-1, 1).numpy()
-        sc_val = dataset.y_proc.scaler_
-        prediction_train = sc_val.inverse_transform(prediction)
-        Y_train_r2 = sc_val.inverse_transform(Y_train_r2)
-        r2_train = r2_score(Y_train_r2, prediction_train)
-        loss_train = running_loss / len(dataset)
+        label = torch.cat(label, dim=0).view(-1, 1)
+        prediction = torch.cat(prediction, dim=0).view(-1, 1)
 
-        file_output('Train MSE: {:.4f}\tTrain R2 score: {:.4f}'.format(loss_train, r2_train))
+    sc_val = dataset.y_proc.scaler_
+    label = sc_val.inverse_transform(label.numpy())
+    prediction = sc_val.inverse_transform(prediction.numpy())
 
-    plt.figure(figsize=(15,12))
+    r2 = r2_score(label, prediction)
+    mse /= len(dataset)
+    mae /= len(dataset)
 
-    plt.plot([0, 200], [0, 200], color='black')
-    plt.scatter(
-        Y_train_r2, prediction_train,
-        c=np.abs(Y_train_r2 - prediction_train), vmax=2.5, vmin=-0., cmap='jet', alpha=0.5
+    file_output(f'MSE: {mse:.4f}\tMAE: {mae:.4f}\tTrain R2 score: {r2:.4f}')
+
+    visualize_regression(
+        label, prediction, mse_score=mse, mae_score=mae, r2_score=r2,
+        plot_max=200, plot_min=0, vmax=40., vmin=0., alpha=0.5, figsize=(15, 12),
+        xlabel='Value', ylabel='Prediction', title='Year of Survival Regression',
+        filename=f"output_train_whole.png", show=False
     )
-    plt.colorbar(label='Error')
-
-    plt.title('logP Regression')
-    plt.xlabel('logP Value')
-    plt.ylabel('Prediction')
-
-    plt.text(
-        100, 0,
-        f'Whole MSE: {loss_train:.4f}\nWhole R2 score: {r2_train:.4f}',
-        fontsize=25,
-        bbox={'boxstyle': 'square', 'ec': (1,1,1), 'fc': (1,1,1), 'linestyle': '--', 'color': 'black'}
-    )
-
-    plt.grid(True)
-    plt.minorticks_on()
-
-    plt.savefig(f"output_train_whole.png")
 
 
 if __name__ == '__main__':
