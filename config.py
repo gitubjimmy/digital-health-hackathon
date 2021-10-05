189085e044140016c08e390a0098760def7594a8
kdha0727 <kdha0727@naver.com>
2021-10-06 07:14:22 +0900

    extra

--- config.py
+++ config.py
@@ -90,31 +90,31 @@ __mutable_attributes__ = ('ACTIVATION', )
 # ==============================================================================
 # Do not change scripts below:
 #
-class FrozenConfig(type(sys)):
-    __doc__ = __doc__
-
-    __INITIALIZED = False
-    __MUTABLE_ATTRIBUTES = __mutable_attributes__[:]
-
-    def __init__(self, name):
-        super().__init__(name)
-        self.__package__ = __package__
-        for k, v in globals().items():
-            if k.isupper():
-                super().__setattr__(k, v)  # Avoid redundant checking by super()
-        self.__INITIALIZED = True
-
-    def __setattr__(self, k, v):
-        if self.__INITIALIZED and not k.startswith('_') and k not in self.__MUTABLE_ATTRIBUTES:
-            raise TypeError("can't set {!r} attribute".format(k))
-        return super().__setattr__(k, v)
-
-    def __delattr__(self, k):
-        if self.__INITIALIZED and not k.startswith('_') and k not in self.__MUTABLE_ATTRIBUTES:
-            raise TypeError("can't delete {!r} attribute".format(k))
-        return super().__delattr__(k)
-
-
-sys.modules[__name__] = FrozenConfig(__name__)
+# class FrozenConfig(type(sys)):
+#     __doc__ = __doc__
 #
+#     __INITIALIZED = False
+#     __MUTABLE_ATTRIBUTES = __mutable_attributes__[:]
+#
+#     def __init__(self, name):
+#         super().__init__(name)
+#         self.__package__ = __package__
+#         for k, v in globals().items():
+#             if k.isupper():
+#                 super().__setattr__(k, v)  # Avoid redundant checking by super()
+#         self.__INITIALIZED = True
+#
+#     def __setattr__(self, k, v):
+#         if self.__INITIALIZED and not k.startswith('_') and k not in self.__MUTABLE_ATTRIBUTES:
+#             raise TypeError("can't set {!r} attribute".format(k))
+#         return super().__setattr__(k, v)
+#
+#     def __delattr__(self, k):
+#         if self.__INITIALIZED and not k.startswith('_') and k not in self.__MUTABLE_ATTRIBUTES:
+#             raise TypeError("can't delete {!r} attribute".format(k))
+#         return super().__delattr__(k)
+#
+#
+# sys.modules[__name__] = FrozenConfig(__name__)
+# #
 # ==============================================================================
